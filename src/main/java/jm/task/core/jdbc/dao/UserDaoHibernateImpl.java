41b14ebf771773a20fd5d76ee5cfd69b9c7901b2
ichurs <chursinov_ivan@mail.ru>
2022-09-18 01:56:00 +0300

    1.1.4 Added Hibernate mapping with annotations in User class. Added getSessionFactory() method in Util class

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,6 +5,20 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final static String CREATE_TABLE = """
+            CREATE TABLE IF NOT EXISTS `1_1_3_ichurs`.`users` (
+                `id` BIGINT(19) NOT NULL AUTO_INCREMENT,
+                `name` VARCHAR(45) NULL DEFAULT NULL,
+                `lastname` VARCHAR(45) NULL DEFAULT NULL,
+                `age` INT NULL DEFAULT NULL,
+                PRIMARY KEY (`id`),
+                UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);""";
+    private final static String ADD_USER = "INSERT INTO users(name, lastname, age) VALUES(?, ?, ?)";
+    private final static String GET_ALL_USERS = "SELECT id, name, lastname, age FROM users";
+    private final static String REMOVE_BY_ID = "DELETE FROM users WHERE id = ?";
+    private final static String CLEAN_TABLE = "TRUNCATE TABLE users";
+    private final static String DROP_TABLE = "DROP TABLE IF EXISTS users";
+
     public UserDaoHibernateImpl() {
 
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
     private static final String URL = "jdbc:mysql://localhost:3306/1_1_3_ichurs";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
+    private static StandardServiceRegistry standardServiceRegistry;
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -18,5 +28,24 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    static {
+        StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+        Map<String, String> dbSettings = new HashMap<>();
+        dbSettings.put(Environment.URL, "jdbc:mysql://localhost:3306/1_1_3_ichurs");
+        dbSettings.put(Environment.USER, "root");
+        dbSettings.put(Environment.PASS, "root");
+        dbSettings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        dbSettings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        registryBuilder.applySettings(dbSettings);
+        standardServiceRegistry = registryBuilder.build();
+        MetadataSources sources = new MetadataSources(standardServiceRegistry);
+        Metadata metadata = sources.getMetadataBuilder().build();
+        sessionFactory = metadata.getSessionFactoryBuilder().build();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
